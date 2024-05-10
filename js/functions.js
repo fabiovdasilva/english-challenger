287c6c8367afeeee9253c870be8d4d28bd173752
fabiovdasilva <fabioviniciusdasilva@outlook.com>
2024-05-10 16:50:15 -0300

    Implementando funcoes

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+var engine = {
+    "cores":['green','purple','pink','red','yellow','orange','grey'],
+    "hexadecimais":{
+        'green': "#02ef00",
+        'purple': "#790093",
+        'black': "#141414",
+        'red': "#e90b08",
+        'yellow': "#e7d703",
+        'orange': "#f16529",
+        'grey': "#ebebeb",
+    },
+
+    "moedas": 0
+}
