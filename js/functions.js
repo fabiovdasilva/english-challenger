c0a8f2d2407a36576c927f030bb6132120936b61
fabiovdasilva <fabioviniciusdasilva@outlook.com>
2024-05-13 16:34:06 -0300

    function sortear cor

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,29 @@
 var engine = {
-    "cores":['green','purple','pink','red','yellow','orange','grey'],
-    "hexadecimais":{
-        'green': "#02ef00",
-        'purple': "#790093",
-        'black': "#141414",
-        'red': "#e90b08",
-        'yellow': "#e7d703",
-        'orange': "#f16529",
-        'grey': "#ebebeb",
-    },
+  cores: ["green", "purple", "pink", "red", "yellow", "orange", "grey"],
+  hexadecimais: {
+    green: "#02ef00",
+    purple: "#790093",
+    black: "#141414",
+    red: "#e90b08",
+    yellow: "#e7d703",
+    orange: "#f16529",
+    grey: "#ebebeb",
+  },
 
-    "moedas": 0
+  moedas: 0,
+};
+
+function sortearCor() {
+  var indexCorSorteada = Math.floor(Math.random() * engine.cores.length);
+  var legendaCorCaixa = document.getElementById("cor-na-caixa");
+
+  legendaCorCaixa.innerText = engine.cores[indexCorSorteada].toUpperCase;
+
+  
+  
 }
+/*  
+Math.random() gera um numero aleatorio, para colocar entre um intervalo apenas multiplicar pelo numero desejado, * engine.cores.length, será um numero aleatorio entre o intervalo do tamanho do vetor cores
+Math.floor retorna um numero inteiro
+
+*/
